0e538de1cec1a3ee04ce0c09dd02970a481b511e
mbeloded <mbeloded@gmail.com>
2025-12-31 21:23:35 +0200

    Fix: Fixed asset and launcher icon configuration.
    - Enabled the assets/logo folder.
    - Removed duplicate flutter_icons.
    - Fixed incorrect image_paths.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,13 +18,13 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.finesplus"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     defaultConfig {
         applicationId = "com.finesplus"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
